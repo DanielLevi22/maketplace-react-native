1d8cd89c4d746facb2fe17e9991f9a19eee3e59e
daniel levi <daniellevi000@gmail.com>
2025-10-23 07:52:50 -0300

    feat: :sparkles: adiciona zustand estado global

--- src/view-models/register/use-register.view-model.tsx
+++ src/view-models/register/use-register.view-model.tsx
@@ -1,9 +1,11 @@
 import { useRegisterMutation } from "@/src/shared/queries/auth/use-register.mutation";
+import { useUserStore } from "@/src/shared/store/use-store";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { registerScheme, type RegisterFormData } from "./resgister.scheme";
 export const useRegisterViewModel = () => {
   const userRegisterMutation = useRegisterMutation();
+  const { setSession } = useUserStore();
   const {
     control,
     handleSubmit,
@@ -21,7 +23,12 @@ export const useRegisterViewModel = () => {
 
   const onSubmit = handleSubmit(async (userData: RegisterFormData) => {
     const { confirmPassword, ...registerData } = userData;
-    await userRegisterMutation.mutateAsync(registerData);
+    const response = await userRegisterMutation.mutateAsync(registerData);
+    setSession({
+      user: response.user,
+      refreshToken: response.refreshToken,
+      token: response.token,
+    });
   });
 
   return {
