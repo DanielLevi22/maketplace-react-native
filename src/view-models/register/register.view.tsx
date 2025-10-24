6619d68fb07812368ddfc0d7eecc8624087d9356
daniel levi <daniellevi000@gmail.com>
2025-10-23 21:09:13 -0300

    style: :lipstick: adiciona componente de input

--- src/view-models/register/register.view.tsx
+++ src/view-models/register/register.view.tsx
@@ -1,7 +1,12 @@
+import { AppInput } from "@/src/shared/components/app-input";
 import type { FC } from "react";
 import { View } from "react-native";
 import type { useRegisterViewModel } from "./use-register.view-model";
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = () => {
-  return <View></View>;
+  return (
+    <View className="flex-1">
+      <AppInput />
+    </View>
+  );
 };
