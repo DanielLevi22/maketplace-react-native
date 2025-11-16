fa0ea5e69b959adf38525c2cbc937eb6161f3197
Daniel Levi <daniellevi000@gmail.com>
2025-11-16 10:13:15 -0300

    fix: :sparkles: ajusta use store pra redirecionamento de rota

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,7 @@ import { Redirect } from "expo-router";
 import { useUserStore } from "../shared/store/use-store";
 
 export default function App() {
-  const { user, token } = useUserStore((state) => ({
-    user: state.user,
-    token: state.token,
-  }));
+  const { user, token } = useUserStore();
 
   if (user && token) {
     return <Redirect href="/(private)/home" />;
